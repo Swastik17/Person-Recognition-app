360a0618dadb5b0a42148effc7c57a695b7638e3
Swastik17 <74951457+Swastik17@users.noreply.github.com>
2020-12-18 11:21:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-Webcam.attach('#camera');
+
 Webcam.set({
     width:350,
     height:300,
     image_format:'png',
     png_quality:90
 });
+Webcam.attach('#camera');
 camera=document.getElementById("camera");
 function take_snapshot(){
     Webcam.snap(function(data_uri){
